89034864560a9d33612f4d20156557308d8a69c4
Xabier <xabier.figueroa96@gmail.com>
2025-03-08 23:17:26 +0100

    Add fast-xml-parser dependency, create EpisodeItem component, and refactor EpisodeList component

--- src/lib/rss.ts
+++ src/lib/rss.ts
@@ -1,21 +1,2 @@
-import { podcastInfo } from "./config";
-import parseFeed from 'rss-to-json'
-
-export interface Show {
-  title: string;
-  description: string;
-  image: string;
-  link: string;
-}
-
-export interface Episode {
-  id?: string;
-  title: string;
-  description: string;
-  episodeImage?: string;
-  episodeNumber?: string;
-  link: string;
-  publishedDate: Date;
-  duration: string;
-  epidose?: string
-}
+import { slugify, truncate } from './utils';
+import podcastConfig from './podcast.config';
---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+export function slugify(text: string) {
+  return text
+  .replace(/([A-Z])/g, '-$1') // Insert dash before uppercase
+  .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,.\/:;<=>?@\[\]^_`{|}~]/g, '')
+  .replace(/[\s_]+/g, '-')
+  .replace(/^-+|-+$/g, '') // Remove leading/trailing dashes
+  .toLowerCase();
+}
+
+export function truncate(str: string, length: number) {
+  const sanitizedString = str.replace(/(<([^>]+)>)/gi, '');
+  if (sanitizedString.length > length) {
+      let truncated = sanitizedString.slice(0, length);
+      if(sanitizedString.charAt(length) !== ' '){
+          truncated = truncated.substring(0, truncated.lastIndexOf(" "));
+      }
+      return `${truncated.trim()}...`;
+  } else return sanitizedString;
+}
